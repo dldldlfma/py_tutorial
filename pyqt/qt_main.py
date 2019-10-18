f8c6bf9db3bfee0ba1db678644f41de0d8be3b18
Sanggun Kim <dldldlfma@gmail.com>
2019-10-18 18:03:13 +0900

    single picture operation

--- pyqt/qt_main.py
+++ pyqt/qt_main.py
@@ -21,7 +21,7 @@ class MainDialog(QDialog):
         self.finder_Btn.clicked.connect(self.file_loader)
         self.save_Btn.clicked.connect(self.file_save)
         self.model = QStandardItemModel()
-        self.file_list = []
+        self.file_list = None
 
         self.Binary_Btn.stateChanged.connect(self.check_state)
         self.Median_Btn.stateChanged.connect(self.check_state)
@@ -45,29 +45,34 @@ class MainDialog(QDialog):
         fname = QFileDialog.getOpenFileName(self)
         self.file_position.setText(fname[0])
         pixmap=QPixmap(fname[0])
+        pixmap = pixmap.scaledToWidth(512)
         self.label.setPixmap(QPixmap(pixmap))
         self.model.appendRow(QStandardItem(fname[0]))
         self.listView.setModel(self.model)
-        self.file_list.append(fname[0])
+        self.file_list = fname[0]
     
     def file_save(self):
         save_position = QFileDialog.getExistingDirectory(self)  #select save folder
         self.save_position.setText(save_position)
+        
+        img=None
 
+        if(self.binary):
+            img = cv.imread(self.file_list, 0)
+        else:
+            img = cv.imread(self.file_list, cv.IMREAD_COLOR)
+        
+        if(self.median):
+            img = cv.medianBlur(img,13)
 
-        for i,value in enumerate(self.file_list):
-            if(self.binary):
-                img = cv.imread(value,0)
-            else:
-                img = cv.imread(value,cv.IMREAD_COLOR)
-            if(self.median):
-                img = cv.medianBlur(img,5)
-            if(self.binary):
-                img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 5)
-            save_path = save_position + '_fix_'+ str(i) + value[value.find('.'):]
-            cv.imwrite(save_path,img)
-            cv.imshow('binary_img',img)
-            cv.waitKey(0)
+        if(self.binary):
+            print(img.shape)
+            img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 5)
+        
+        save_path = save_position + '/fix_' + self.file_list[self.file_list.find('.'):]
+        cv.imwrite(save_path,img)
+        cv.imshow('binary_img',img)
+        cv.waitKey(0)
 
 if __name__ =='__main__':
     app = QApplication(sys.argv)
